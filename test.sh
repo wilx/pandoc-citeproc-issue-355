896593b3c142549655548d5bdaf293dc43e2e14f
Vaclav Haisman <vhaisman@gmail.com>
2019-01-04 02:18:18 +0100

    Add no space example.

--- test.sh
+++ test.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+set -x
+
 diff -ud ./iso690-numeric-cs.csl ./iso690-numeric-cs-wilx.csl
 
 pandoc -f markdown -t native --bibliography citace.yaml --csl ./iso690-numeric-cs.csl -o original.native test.md
@@ -7,3 +9,11 @@ pandoc -f markdown -t native --bibliography citace.yaml --csl ./iso690-numeric-c
 pandoc -f markdown -t native --bibliography citace.yaml --csl ./iso690-numeric-cs-wilx.csl -o modified.native test.md
 
 diff -ud original.native modified.native | wdiff -td
+
+
+
+diff -ud ./iso690-numeric-cs.csl ./iso690-numeric-cs-no-space.csl
+
+pandoc -f markdown -t native --bibliography citace.yaml --csl ./iso690-numeric-cs-no-space.csl -o no-space.native test.md
+
+diff -ud original.native no-space.native | wdiff -td
